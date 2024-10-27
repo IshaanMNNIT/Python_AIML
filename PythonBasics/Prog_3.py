7a27d62111395045bb85c684603ccb310b2e6bda
IshaanMNNIT <singhishaan2004@gmail.com>
2024-10-27 18:47:28 +0530

    Practicing

--- PythonBasics/Prog_3.py
+++ PythonBasics/Prog_3.py
@@ -1,1 +1,20 @@
 # String Compression
+s="aabcccccaaa"
+print(s)
+i=0
+t=""
+count = 0
+while i<len(s):
+    t=t+s[i]
+    j=i
+    while j<len(s)-1:
+        if s[j]==s[j+1]:
+            count+=1
+            j+=1
+        else:
+            i=j+1
+            break
+    i=j+1
+    t+=str(count+1)
+    count=0
+print(t)
