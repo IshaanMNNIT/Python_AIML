73d758a5e895ca669ae323d3337ca5cbd9d01acb
IshaanMNNIT <singhishaan2004@gmail.com>
2024-10-27 09:21:10 +0530

    Practicing

--- PythonBasics/Prog_2.py
+++ PythonBasics/Prog_2.py
@@ -1,20 +1,16 @@
 s = "My userID is john17 and my 4 digit pin is 1234 which is secret."
 t = ""
-
 for i in range(len(s)):
-    if s[i].isalpha():  # If character is a letter, add it to t
+    if s[i].isalpha():
         t += s[i]
-    elif not s[i].isdigit():  # If it's not a letter or digit (e.g., space, punctuation), add it
+    elif not s[i].isdigit():
         t += s[i]
-    elif s[i].isdigit():  # If character is a digit
-        # Check if it's part of a word or a standalone number
-        if (i > 0 and s[i - 1].isalpha()) or (i < len(s) - 1 and s[i + 1].isalpha()):
-            t += s[i]  # Keep it if it's part of a word
+    elif s[i].isdigit():
+        # Check if digit is part of a word by examining neighboring characters
+        if i>0 and t[i-1].isdigit():
+            t += s[i]
+        elif (i > 0 and s[i - 1].isalpha()) or (i < len(s) - 1 and s[i + 1].isalpha()):
+            t += s[i]  # Keep digits within words unchanged
         else:
-            t += 'x'  # Replace with 'x' if it's part of a standalone number
-
+            t += 'x'  # Replace standalone numbers with 'x'
 print(t)
-
-
-
-
