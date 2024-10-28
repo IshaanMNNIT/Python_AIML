4ec6d38b38202bbda835b9bb3c2f526c27ab6253
IshaanMNNIT <singhishaan2004@gmail.com>
2024-10-28 19:43:02 +0530

    Practicing

---
+++ PythonBasics/matchtut.py
@@ -0,0 +1,12 @@
+name = input("Enter the name of the character : ")
+match name :
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who ?")
---
+++ PythonBasics/matchtut2.py
@@ -0,0 +1,8 @@
+name = input("Enter the name of the character : ")
+match name :
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who ?")
